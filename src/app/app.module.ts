adfa1efdc8fde895a79dbca2605cc19221f37b49
Carolyn <ccpianist@gmail.com>
2019-02-22 14:19:10 -0800

    link movie-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, MoviesComponent],
+  declarations: [AppComponent, MoviesComponent, MovieDetailComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
