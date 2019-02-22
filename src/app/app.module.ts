0a93ec8249c66697b5635b16b5d17c0878bcc3ef
Carolyn <ccpianist@gmail.com>
2019-02-22 13:26:43 -0800

    add movie name imput & import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MoviesComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MoviesComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
