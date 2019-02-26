6bec80b9d7c242c9e4e9ae23fc4f831391b5ab4e
Carolyn <ccpianist@gmail.com>
2019-02-26 15:28:14 -0800

    folder structure: Client & Server

---
+++ Server/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MagnificentMovieMenu.movies
+{
+    public partial class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Year { get; set; }
+        public string Category { get; set; }
+        public int? Rating { get; set; }
+    }
+}
