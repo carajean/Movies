946774aaa19ce786d28a48cea7a5b97711197285
Carolyn <ccpianist@gmail.com>
2019-02-27 18:20:38 -0800

    restore dependencies

---
+++ Test/movies/List.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MagnificentMovieMenu.movies
+{
+    public partial class List
+    {
+        public int Id { get; set; }
+        public string List1 { get; set; }
+    }
+}
