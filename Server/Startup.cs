31c3eb8727362339d30b7bed70d755e57f9d2784
Carolyn <ccpianist@gmail.com>
2019-02-27 16:41:32 -0800

    cleanup dotnet build

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -17,60 +17,60 @@ using Microsoft.Extensions.Options;
 
 namespace MagnificentMovieMenu
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<moviesContext>(options =>
-                options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<moviesContext>()
-                .AddDefaultTokenProviders();
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/dist";
-            });
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseStaticFiles();
-
-            //app.UseSpaStaticFiles();
-            //app.UseSpa(spa =>
-            //{
-            //    spa.Options.SourcePath = "ClientApp";
-            //    if (env.IsDevelopment())
-            //    {
-            //        spa.UseAngularCliServer(npmScript: "start");
-            //    }
-            //});
-
-            app.UseCors(options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials());
-            app.UseHttpsRedirection();
-            app.UseMvc();
-
-            //dbContext.Database.EnsureCreated();
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<moviesContext>(options =>
+          options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+      services.AddIdentity<IdentityUser, IdentityRole>()
+          .AddEntityFrameworkStores<moviesContext>()
+          .AddDefaultTokenProviders();
+      services.AddSpaStaticFiles(configuration =>
+      {
+        configuration.RootPath = "Client/dist";
+      });
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      app.UseStaticFiles();
+
+      //app.UseSpaStaticFiles();
+      //app.UseSpa(spa =>
+      //{
+      //    spa.Options.SourcePath = "ClientApp";
+      //    if (env.IsDevelopment())
+      //    {
+      //        spa.UseAngularCliServer(npmScript: "start");
+      //    }
+      //});
+
+      app.UseCors(options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials());
+      app.UseHttpsRedirection();
+      app.UseMvc();
+
+      //dbContext.Database.EnsureCreated();
+    }
+  }
 }
