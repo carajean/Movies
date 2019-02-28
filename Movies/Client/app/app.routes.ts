20befe69d04f50eed06edc61e7d9f8e4bf117369
Carolyn <ccpianist@gmail.com>
2019-02-28 11:18:27 -0800

    new angularwebapi

--- Movies/Client/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { RouterModule, Routes } from '@angular/router';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'menu', pathMatch: 'full' },
-  { path: 'about', loadChildren: './about/about.module#AboutModule' }
-];
-
-export const AppRoutes = RouterModule.forRoot(routes);
