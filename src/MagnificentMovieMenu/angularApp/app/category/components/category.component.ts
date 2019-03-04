52c3458420cc02ca4bc5ee1166afa9a7fc029a59
Carolyn <ccpianist@gmail.com>
2019-03-03 16:03:40 -0800

    can rate movies from category view

--- src/MagnificentMovieMenu/angularApp/app/category/components/category.component.ts
+++ src/MagnificentMovieMenu/angularApp/app/category/components/category.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 // import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
@@ -169,6 +169,46 @@ export class CategoryComponent implements OnInit {
       error => {
         console.log(error);
       }
+    );
+  }
+
+  @Input() rating: number;
+  @Output() ratingClick: EventEmitter<any> = new EventEmitter<any>();
+
+  inputName: string;
+  id: number;
+
+  onClick(rating: number, slugStr: string): void {
+    this.rating = rating;
+    this.dataService.getAll().subscribe(
+      data => (
+        data.filter(m => m.slug === slugStr),
+        this.movies.forEach(m => {
+          this.id = m.id;
+          this.dataService
+            .update(this.id, {
+              id: this.id,
+              name: m.name,
+              year: m.year,
+              category: m.category,
+              slug: m.slug,
+              img: m.img,
+              rating
+            })
+            .subscribe(
+              () => {
+                this.getMoviesByCategory();
+                this.movie = new Movie();
+              },
+              error => {
+                console.log(error);
+              }
+            ),
+            this.ratingClick.emit({
+              rating: rating
+            });
+        })
+      )
     );
   }
 }
