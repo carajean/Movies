168427f3e9fbb85fff93b07ae51612f9dafdee72
Carolyn <ccpianist@gmail.com>
2019-03-03 22:46:13 -0800

    edit About component

--- src/MagnificentMovieMenu/angularApp/app/about/components/about.component.ts
+++ src/MagnificentMovieMenu/angularApp/app/about/components/about.component.ts
@@ -10,7 +10,7 @@ export class AboutComponent {
 
   constructor() {
     this.message =
-      'This project has been a fun and challenging exploration of the ASP.NET Web Api backend, alongside an Angular frontend framework, with data housed in a MySQL database and altogether compiled by Webpack. My programming background comes from the "NERP" stack (Node, Express, React/Redux, PostgreSQL). I\'m excited to have reinforced my full-stack programming knowledge, especially for handling code in C# and TypeScript, both of which were relatively new to me before embarking on this project.';
+      'This project has been a fun and challenging exploration of the ASP.NET Web Api backend, alongside an Angular frontend framework, with data housed in a MySQL database and altogether compiled by Webpack. My programming background comes from the "NERP" stack (Node, Express, React/Redux, PostgreSQL). I\'m excited to have reinforced my fullstack programming knowledge, especially for handling REST-ful code in C# and TypeScript, and learning to deploy via Azure.';
     this.message2 = "What's next? Here are my ideas:";
   }
 }
